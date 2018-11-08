ce63054a38580124640a9e0ffaab058edafe4e80
twhy2000 <twhg2000@gmail.com>
2018-11-07 20:11:59 -0800

    daytwo

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// var name ="tyreese"; this
+//
+// var luckynumber=42
+// const firstname ="tyreese"
+// {
+//   const name= "nathan"
+//   console.log(name);
+// }
+// console.log(name);
+// console.log("marisol",firstName)
+// const = greeting="hello my name is";
+// const = name "Bob";
+//
+// console.log(greeting + name)
+// const name  ="bob";
+//
+// console.log(`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi sit, atque corporis. Veniam consequatur, error recusandae omnis ${name} nesciunt saepe, sequi repellat vitae, expedita nemo excepturi quibusdam perferendis at libero corporis`);
+//
+// const date = new Date('');
+//
+// console.log(date);
+//
+// const str = ''
+//
+// console.log(str.length);
+//
+// const num = new Number(42);
+// const num2 = 42;
+//
+// const array = [1,3]
+// const array2 = new Array(1,3)
+// const arrayMixed = ['Ben' 29, true];
+// console.log(arrayMixed);
+//
+// const obj ={
+//   name:'Ben',
+//   age: 29
+// }
+// console.log(obj);
+// const response = confirm('are you server');
+//
+// console.log(response);
+// const favNum = prompt('what is your favorite number?')
+//
+// const realnum = parseInt(favNum);
+//
+// const num = 55;
+//
+//
+//
+// console.log(num.toString());
