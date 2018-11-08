fdad919525f8b6ce89b0ecb73ae2e228039ad4f4
twhy2000 <twhg2000@gmail.com>
2018-11-07 21:02:50 -0800

    day3 some note

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// const firstName = prompt('whats your name');
+// const lastName = prompt('whats your lastName');
+//
+// console.log(firstName);
+//
+// alert ('lastName')
+//
+// const bd = prompt('what is your birthday');
+//
+// console.log(bd);
+// confirm(`Is your birthday correct:${birthday}`)
+// console.log ( 1===true );
+const obj1 = {
+  name: 'Ben'
+};
+
+const obj2 = {
+  name: 'Ben'
+};
+
+const arr1 ['cat']
+const arr2 ['cat']
+
+console.log(arr1===arr2);
